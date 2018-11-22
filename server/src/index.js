99ad57f2ec18e0e3d49067c80a16d023d1a90dff
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-22 09:14:35 +0530

    Added renderToString.  TODO: Fix jsx error

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
+// ROOT file of server
+
 const express = require('express');
 const app = express();
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/home').default;
 
-app.get('/', (req, res) => { });
+app.get('/', (req, res) => {
+    const content = renderToString(<Home />);
+    res.send(content);
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
