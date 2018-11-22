a6f32ed0a1c5aa4f4f012920a279d4fe75befa6e
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-22 10:51:38 +0530

    Add npm script to run all scripts in parallel. Note: change the scripts key to dev:build-server etc

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -27821,7 +27821,7 @@ var Home = function Home() {
         _react2.default.createElement(
             "h2",
             null,
-            "Home>"
+            "Home is where Heart is>"
         ),
         _react2.default.createElement(
             "button",
